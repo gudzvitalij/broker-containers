744b86e63253b608c64fe4a42dc7eb46e95ad2b4
gudzvitalij <enigmmm@yandex.ru>
2023-03-26 18:45:10 +0300

    getService, sendService

---
+++ src/main/java/service/SendService.java
@@ -0,0 +1,27 @@
+package service;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.List;
+import java.util.Properties;
+
+public class SendService {
+    private String topicName;
+    private Properties properties;
+
+    public SendService(String bootstrapServers, String topicName) {
+        this.topicName = topicName;
+        properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+    }
+
+    public void senRecords(List<String> records) {
+        KafkaProducer producer = new KafkaProducer(properties);
+        records.forEach(s ->producer.send(new ProducerRecord(topicName,s)));
+    }
+}
